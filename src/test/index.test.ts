ca40cc06fc0ba05e02601e31a2550c15e85361a3
tascript <wataruscript8611@gmail.com>
2023-09-22 21:06:48 +0900

    test request for POST /profile

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -5,4 +5,35 @@ describe('Test API', () => {
     const res = await app.request('/')
     expect(res.status).toBe(200)
   })
+
+  describe('POST /profile' , () => {
+    test('return 400 when payload is invalid', async () => {
+      const method = 'POST'
+      const body = JSON.stringify({
+        name: 'tascript',
+        age: undefined
+      })
+      const res = await app.request('/profile', {
+        method,
+        body
+      })
+      expect(res.status).toBe(400)
+    })
+
+    test('return 201', async () => {
+      const method = 'POST'
+      const body = JSON.stringify({
+        name: 'tascript',
+        age: 34
+      })
+      const res = await app.request('/profile', {
+        method,
+        body
+      })
+      expect(res.status).toBe(201)
+      expect(JSON.parse(await res.text())).toEqual({
+        message: "tascript turned 34!"
+      })
+    })
+  })
 })
