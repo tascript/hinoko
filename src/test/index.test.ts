c7ba9815c2630c46b0d8d812d60658c2988cacc0
tascript <wataruscript8611@gmail.com>
2023-11-20 00:02:28 +0900

    prepare utils

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,5 +1,6 @@
 import app from '../index'
 import { expect, test, describe, spyOn } from "bun:test"
+import { genRequest } from './utils/request'
 import * as Account  from '../usecase/account'
 
 describe('Test API', () => {
@@ -10,32 +11,26 @@ describe('Test API', () => {
 
   describe('POST /accounts' , () => {
     test('return 400 when payload is invalid', async () => {
-      const method = 'POST'
-      const headers = { "Content-Type": "application/json" }
-      const body = JSON.stringify({
-        name: 'tascript',
-        age: undefined
-      })
-      const res = await app.fetch(new Request ('http://localhost/accounts', {
-        method,
-        headers,
-        body
+      const res = await app.fetch(genRequest({
+        path: '/accounts',
+        method: 'POST',
+        body: JSON.stringify({
+          name: 'tascript',
+          age: undefined
+        })
       }))
       expect(res.status).toBe(400)
     })
 
     test('return 201', async () => {
-      const method = 'POST'
-      const headers = { "Content-Type": "application/json" }
-      const body = JSON.stringify({
-        name: 'tascript',
-        age: 34
-      })
       const mockSaveAccount = spyOn(Account, 'saveAccount').mockImplementation(() => new Promise ((res) => res(undefined)))
-      const res = await app.fetch(new Request ('http://localhost/accounts', {
-        method,
-        headers,
-        body
+      const res = await app.fetch(genRequest({
+        path: '/accounts',
+        method: 'POST',
+        body: JSON.stringify({
+          name: 'tascript',
+          age: 34
+        })
       }), {
         DB: 'test'
       })
---
+++ src/test/utils/request.ts
@@ -0,0 +1,13 @@
+interface GenRequest {
+  path: string
+  method: 'GET' | 'POST' | 'PUT' | 'DELETE',
+  body: string
+}
+
+export const genRequest = (arg: GenRequest) => {
+  return new Request('http://localhost' + arg.path, {
+    method: arg.method,
+    headers: { "Content-Type": "application/json" },
+    body: arg.body
+  })
+}
