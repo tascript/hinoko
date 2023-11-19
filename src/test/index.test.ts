bf182e3c4c934c371ebd82b7875e56b23502f005
tascript <wataruscript8611@gmail.com>
2023-11-19 23:32:20 +0900

    fix test

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -1,5 +1,5 @@
 import app from '../index'
-import { expect, test, describe, spyOn, jest, mock } from "bun:test"
+import { expect, test, describe, spyOn } from "bun:test"
 import * as Account  from '../usecase/account'
 
 describe('Test API', () => {
@@ -11,36 +11,36 @@ describe('Test API', () => {
   describe('POST /accounts' , () => {
     test('return 400 when payload is invalid', async () => {
       const method = 'POST'
+      const headers = { "Content-Type": "application/json" }
       const body = JSON.stringify({
         name: 'tascript',
         age: undefined
       })
-      const res = await app.request('/accounts', {
+      const res = await app.fetch(new Request ('http://localhost/accounts', {
         method,
+        headers,
         body
-      })
+      }))
       expect(res.status).toBe(400)
     })
 
     test('return 201', async () => {
       const method = 'POST'
+      const headers = { "Content-Type": "application/json" }
       const body = JSON.stringify({
         name: 'tascript',
         age: 34
       })
-      // FIXME: Not working for ESM spyOn
       const mockSaveAccount = spyOn(Account, 'saveAccount').mockImplementation(() => new Promise ((res) => res(undefined)))
-      const res = await app.request('/accounts', {
+      const res = await app.fetch(new Request ('http://localhost/accounts', {
         method,
+        headers,
         body
-      }, {
+      }), {
         DB: 'test'
       })
       expect(mockSaveAccount).toHaveBeenCalled()
       expect(res.status).toBe(201)
-      expect(await res.json()).toEqual({
-        message: "Created"
-      })
     })
   })
 })
